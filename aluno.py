80ae53d1753e3a3cffd0ca7598bbc3121523ff40
gabrielzomer <81660099+gabrielzomer@users.noreply.github.com>
2023-10-26 20:03:51 -0300

    ajustes aluno

--- aluno.py
+++ aluno.py
@@ -2,3 +2,6 @@ class Aluno:
     def __init__(self, matricula):
         self._matricula = matricula
         self.curso = None
+        
+    def escolher_curso(self, curso):
+        self.curso = curso
