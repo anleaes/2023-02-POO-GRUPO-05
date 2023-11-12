d5973c24a6fd3014a8907798efde7f52467847fb
gabrielzomer <81660099+gabrielzomer@users.noreply.github.com>
2023-11-12 16:36:23 -0300

    Delete ex01 directory

--- ex01/curso.py
+++
@@ -1,5 +0,0 @@
-class Curso:
-    def __init__(self, nome, carga_horaria, preco):
-        self.nome = nome
-        self.carga_horaria = carga_horaria
-        self.preco = preco
--- ex01/livro.py
+++
@@ -1,4 +0,0 @@
-class Livro:
-    def __init__(self, titulo, autor):
-        self.titulo = titulo
-        self.autor = autor
