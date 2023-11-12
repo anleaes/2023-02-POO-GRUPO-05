83ba13a4ffc7f143742824be8c7f4a400992df99
gabrielzomer <81660099+gabrielzomer@users.noreply.github.com>
2023-11-12 17:27:50 -0300

    criação main atv2 e import

---
+++ atv02/main.py
@@ -0,0 +1,8 @@
+from category import Category
+from product import Product
+from order_item import OrderItem
+from order import Order
+from client import Client
+from client_socialnetwork import ClientSocialnetwork
+from socialnetwork import Socialnetwork
+
