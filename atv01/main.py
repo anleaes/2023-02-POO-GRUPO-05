6f1c92ef538fd8d28905c478d54f993855e79a13
gabrielzomer <81660099+gabrielzomer@users.noreply.github.com>
2023-11-12 17:34:21 -0300

    criação main e importações

---
+++ atv01/main.py
@@ -0,0 +1,7 @@
+from pessoa import Pessoa
+from pessoa_fisica import PessoaFisica
+from pessoa_juridica import PessoaJuridica
+from aluno import Aluno
+from curso import Curso
+from inscricao import Inscricao
+
