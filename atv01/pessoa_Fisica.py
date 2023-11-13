764a2160fc8b5141df2d01b650c51653f2f7dbe4
RaphaelGab <aza.gabardo@gmail.com>
2023-11-12 21:48:13 -0300

    Criada classe Pessoa Fisica

---
+++ atv01/pessoa_Fisica.py
@@ -0,0 +1,7 @@
+from pessoa import Pessoa
+
+class PessoaFisica(Pessoa):
+    def __init__(self, cpf, data_nascimento, pessoa_nome, pessoa_endereco):
+        Pessoa.__init__(self, pessoa_nome, pessoa_endereco)
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
