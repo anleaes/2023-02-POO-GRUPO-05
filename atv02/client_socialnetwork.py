9d8cb2f47e492608e425b6e131cea9f53abb0b32
RLD5 <101642643+RLD5@users.noreply.github.com>
2023-12-06 18:13:45 -0300

    Delete atv02 directory

--- atv02/client_socialnetwork.py
+++
@@ -1,4 +0,0 @@
-class ClientSocialnetwork:
-    def __init__(self, client, socialnetwork):
-        self.client = client
-        self.socialnetwork = socialnetwork
--- atv02/socialNetwork.py
+++
@@ -1,5 +0,0 @@
-class SocialNetwork:
-    
-    def __init__(self, name, description):
-        self.name : name
-        self.description = description
