8cb0fa30d231bc326da7feb968c429b9bcbfe70e
RLD5 <101642643+RLD5@users.noreply.github.com>
2023-12-06 18:12:49 -0300

    Delete atv_old directory

--- atv_old/aluno.py
+++
@@ -1,7 +0,0 @@
-class Aluno:
-    def __init__(self, matricula):
-        self._matricula = matricula
-        self.curso = None
-        
-    def escolher_curso(self, curso):
-        self.curso = curso
