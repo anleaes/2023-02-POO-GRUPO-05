7d214241dd596bb5a2bfd09561a4b51835b7a52f
RLD5 <101642643+RLD5@users.noreply.github.com>
2023-12-06 18:13:35 -0300

    Delete atv01 directory

--- atv01/inscricao.py
+++
@@ -1,4 +0,0 @@
-class Inscricao:
-    def __init__(self, aluno, curso):
-        self.aluno = aluno
-        self.curso = curso
--- atv01/pessoa_juridica.py
+++
@@ -1,9 +0,0 @@
-from pessoa import Pessoa
-
-class PessoaJuridica(Pessoa):
-    def __init__(self, cnpj, razao_social, data_abertura, pessoa_nome, pessoa_endereco):
-        self.cnpj = cnpj
-        self.razao_social = razao_social
-        self.data_abertura = data_abertura
-
-# Usando a classe pessoa para herdar os itens.
